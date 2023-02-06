d6e16679c926dda09cbbc0c368bc2c51ee6e37f8
Adam-Rohde <adamrohde@ucla.edu>
2023-02-06 12:37:53 -0800

    documented funk

--- R/ripp.R
+++ R/ripp.R
@@ -1,3 +1,13 @@
+#' test funk
+#'
+#' @param x anything
+#'
+#' @return prints "testing Funk" and x
+#' @export
+#'
+#' @examples
+#' x <- 2
+#' funk(x)
 funk <- function(x){
   print("testing Funk")
   print(x)
