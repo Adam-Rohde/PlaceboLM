7a07e57f7044bbb47ead985f5807b5905a6a4314
Adam-Rohde <adamrohde@ucla.edu>
2023-02-06 16:55:03 -0800

    add revised_estimates functions

---
+++ R/revised_estimate.R
@@ -0,0 +1,42 @@
+#' @export
+revised_estimate <- function(type = c("placebo outcome","placebo treatment","double placebo"),
+                 ...){
+  type <- match.arg(type)
+  switcher <- switch(type,
+                     "placebo outcome" = revised_estimate.p.outcome,
+                     "placebo treatment" = revised_estimate.p.treatment,
+                     "double placebo" = revised_estimate.double.p)
+  switcher(...)
+}
+
+
+#' @export
+revised_estimate.p.outcome <- function(type,ripped,
+                           gamma,
+                           lambda,
+                           beta.nd.pxz) {
+  beta.yd.px = ripped$stats$beta.yd.px
+  revised_estimate = beta.yd.px - bias(type,gamma,lambda,beta.nd.pxz)
+  return(revised_estimate)
+}
+
+#' @export
+revised_estimate.p.treatment <- function(type,ripped,
+                             gamma,
+                             lambda,
+                             beta.yp.dxz) {
+  beta.yd.px = ripped$stats$beta.yd.px
+  revised_estimate = beta.yd.px - bias(type,gamma,lambda,beta.yp.dxz)
+  return(revised_estimate)
+}
+
+
+#' @export
+revised_estimate.double.p <- function(type,ripped,
+                          beta.yp.dxz,
+                          beta.nd.pxz,
+                          beta.np.dxz) {
+  beta.yd.px = ripped$stats$beta.yd.px
+  revised_estimate = beta.yd.px - bias(type,beta.yp.dxz,beta.nd.pxz,beta.np.dxz)
+  return(revised_estimate)
+}
