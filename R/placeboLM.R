4a0c62823651654647fa2ed86589a817c32234cb
Adam-Rohde <adamrohde@ucla.edu>
2024-07-17 09:40:19 -0700

    revise line chart legend

--- R/placeboLM.R
+++ R/placeboLM.R
@@ -732,15 +732,24 @@ placeboLM_line_plot <- function(plm,bootstrap=TRUE,n_boot=10,ptiles = c(0,0.5,1)
       if(gr1[gr1[,focus_param] == x,"Estimate"]<=0){y = max} else {y = min}
 
 
-      graphics::text(paste0(intToUtf8(9632)," DID (m=1, k=",round(kDID,3),") Estimate = ",round(DID_estimate,decimals)),
-                     x=x,
-                     y=y + s*0*range,col="darkgreen",adj=1)
-      graphics::text(paste0(intToUtf8(9650)," Perfect Placebo, k=1 Estimate = ",round(DID_k1_estimate,decimals)),
-                     x=x,
-                     y=y + s*0.05*range,col="blue",adj=1)
-      graphics::text(paste0(intToUtf8(9670)," No Unobserved Confounding Estimate = ",round(SOO_estimate,decimals)),
-                     x=x,
-                     y=y + s*0.1*range,col="navy",adj=1)
+      graphics::legend(legend=
+                         c(paste0(intToUtf8(9632)," DID (m=1, k=",round(kDID,3),") Estimate = ",round(DID_estimate,decimals)),
+                           paste0(intToUtf8(9650)," Perfect Placebo, k=1 Estimate = ",round(DID_k1_estimate,decimals)),
+                           paste0(intToUtf8(9670)," No Unobserved Confounding Estimate = ",round(SOO_estimate,decimals))
+                         ),
+                       x=x,
+                       y=y + s*0.1*range,text.col=c("darkgreen","blue","navy"),adj=1,xjust =1, bg = "white")
+
+
+      # graphics::text(paste0(intToUtf8(9632)," DID (m=1, k=",round(kDID,3),") Estimate = ",round(DID_estimate,decimals)),
+      #                x=x,
+      #                y=y + s*0*range,col="darkgreen",adj=1)
+      # graphics::text(paste0(intToUtf8(9650)," Perfect Placebo, k=1 Estimate = ",round(DID_k1_estimate,decimals)),
+      #                x=x,
+      #                y=y + s*0.05*range,col="blue",adj=1)
+      # graphics::text(paste0(intToUtf8(9670)," No Unobserved Confounding Estimate = ",round(SOO_estimate,decimals)),
+      #                x=x,
+      #                y=y + s*0.1*range,col="navy",adj=1)
 
     }
 
